cd650222de804881b9f018622a23fcf0c5395159
Grzegorz Dec <grzegorzdec90@gmail.com>
2018-04-24 09:43:26 +0200

    cleanup

--- Android/app/src/main/java/com/midrive/learnerapp/model/GeoJSON.kt
+++
@@ -1,5 +0,0 @@
-package com.midrive.learnerapp.model
-
-import io.realm.RealmObject
-
-open class GeoJSON(var lon: Long? = 0, var lat: Long? = 0) : RealmObject()
--- Android/app/src/main/java/com/midrive/learnerapp/model/Status.kt
+++
@@ -1,5 +0,0 @@
-package com.midrive.learnerapp.model
-
-import io.realm.RealmObject
-
-open class Status(var status: String? = ""): RealmObject()
